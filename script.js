4697def9d82256438593491e7832c4dee956284b
gokuss235 <43241764+gokuss235@users.noreply.github.com>
2019-03-07 09:44:01 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#first").click(function(){
+ $(".star").fadeToggle();
+});
+
+$("#second").click(function(){
+ $(".powerup").slideToggle();
+});
+
+$("#third").click(function(){
+ $(".instant").toggle();
+});
